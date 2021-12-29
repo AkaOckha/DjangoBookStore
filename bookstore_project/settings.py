4cf944cc15e4a5b2801a2f1ed0639d90d760cba5
AkaOckha <yakopl@hotmail.com>
2021-12-29 17:09:57 +0300

    first commit

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Local
+    'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
 ]
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
