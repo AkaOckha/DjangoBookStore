fbb5912dffe0569a22e0bf187be90b8867bf6233
AkaOckha <yakopl@hotmail.com>
2021-12-30 11:26:44 +0300

    ch5

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+import imp
+from unicodedata import name
+from django.urls import path
+
+from .views import SignupPageView
+
+urlpatterns = [
+    path('signup/', SignupPageView.as_view(), name='signup'),
+]
